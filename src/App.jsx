eb7c4f4edb234fd35cd029e088f16f365c2e1e53
mr9 asif <asifalibd002@gmail.com>
2024-03-13 23:45:19 +0600

    added all header with data

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from './Components/Header'
+import './App.css'
+import Blogs from './Components/Blogs'
+
+function App() {
+  
+
+  return (
+    <>
+     <Header></Header> 
+     <Blogs></Blogs>
+    
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+    return (
+        <div className="max-w-[1400px] px-8 flex justify-between items-center  mx-auto h-20 bg-blue-300">
+            <h1 className="text-4xl text-red-400 font-bold">Knowledge Cafe</h1>
+            <img src="/image/profile.png" alt="" />
+        </div>
+    );
+};
+
+export default Header;
